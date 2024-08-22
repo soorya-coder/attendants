58337e04083bc45a3ecd9a161fb486e7558154db
soorya <sooryasivask@gmail.com>
2024-08-23 00:35:41 +0530

    attend prototype

--- lib/screen/attend.dart
+++ lib/screen/attend.dart
@@ -392,7 +392,7 @@ class _AttendbodyState extends State<Attendbody> {
                   );
                 }),
 
-            
+
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
